b9e5d4caaf0ba75103ca71dbef25fe349200e350
Rakibul2580 <rh565153@gmail.com>
2022-09-03 20:58:30 +0300

    almost com

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 const loadData = async() => {
-    const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
-    const data = await res.json()
-    return(data.data.news_category)
+    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    try{
+        const res = await fetch(url)
+        const data = await res.json()
+        return(data.data.news_category)
+    }
+    catch (error){
+        console.log(error)
+    }
 }
 const DetailData = async(id) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
-    const data = await res.json()
-    return(data.data)
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    try{
+        const res = await fetch(url)
+        const data = await res.json()
+        return(data.data)
+    }
+    catch (error){
+        console.log(error)
+    }
 }
 const modalData = async(id) =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/${id}`)
-    const data = await res.json()
-    return(data.data[0])
+    const url = `https://openapi.programming-hero.com/api/news/${id}`
+    try{
+        const res = await fetch(url)
+        const data = await res.json()
+        return(data.data[0])
+    }
+    catch (error){
+        console.log(error)
+    }
 }
 const displyNav = async() => {
     const data = await loadData()
     const nav = document.getElementById('nav-detail')
+    nav.innerHTML = `
+        <li><a class="nav-link px-4 mx-2" href="#">Home</a>
+    `
     data.forEach(data => {
         const {category_id, category_name} = data
         const li = document.createElement('li')
         li.classList.add('nav-item')
         li.innerHTML = `
-            <a onclick="displyDetail('${category_id}')" class="nav-link px-4 mx-3" href="#">${category_name}</a>
+            <a onclick="displyDetail('${category_id}')" id="nav" class="nav-link px-4 mx-2" href="#">${category_name}</a>
         `
         nav.appendChild(li)
     });
@@ -31,7 +52,11 @@ displyNav()
 const displyDetail = async(id) => {
     document.getElementById('spinner').classList.remove('d-none')
     const data = await DetailData(id)
-    document.getElementById('items').innerText = `${data.length}  items found for category Entertainment`
+    if(data.length > 0){
+        document.getElementById('items').innerText = `${data.length}  items found for category Entertainment`
+    }else{
+        document.getElementById('items').innerText = `Now found for category Entertainment`
+    }
     const careDetail = document.getElementById('card')
     careDetail.textContent = ''
     data.forEach(data => {
@@ -46,7 +71,7 @@ const displyDetail = async(id) => {
                     <h5 class="card-title">${title}</h5>
                     <p class="card-text">${details.slice(0, 350)}...</p>
                     <div class="d-flex justify-content-between align-items-center mt-4">
-                        <div class="d-flex align-items-center w-25 gap-2">
+                        <div class="d-flex align-items-center w-50 gap-2">
                             <img src="${img}" class="w-25 rounded-circle mr-2" alt="">
                             <h6 class="my-2">${name ? name : 'Not Found Name'}</h6>
                         </div>
@@ -71,6 +96,7 @@ const displyDetail = async(id) => {
 
 const newsDetail = async(newsId) =>{
     const data = await modalData(newsId)
+    console.log(data)
     const modalDetail = document.getElementById('exampleModal')
     modalDetail.innerHTML = `
     <div class="modal-dialog">
@@ -80,9 +106,10 @@ const newsDetail = async(newsId) =>{
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <img src="${data ? data.image_url : 'Not Found image' }" alt="" class="img-fluid rounded-start">
-      <p class="mt-3">${data ? data.details.slice(0, 200)
-        : 'Not Found Tital'}...</p>
+      <img src="${data ? data.image_url : 'Not Found image' }" alt="Not Found image" class="img-fluid rounded-start">
+      <div class="d-flex align-items-center w-50 gap-2">
+        <img src="${data ? data.author.img : 'Not Found image'}" class="w-25 rounded-circle mr-2" alt="Not Found image">
+        <h6 class="my-2">${data?.author?.name ? data.author.name : 'Not Found Name'}</h6>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
       </div>
@@ -91,3 +118,9 @@ const newsDetail = async(newsId) =>{
     `
 }
 newsDetail()
+
+// const num = [20, 22, 5, 45, 15]
+// num.sort(function(a,b){
+//     return a - b
+// })
+// console.log(num)
